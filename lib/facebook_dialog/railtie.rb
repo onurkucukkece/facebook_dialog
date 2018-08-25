7ea90955480c79fb07a0ca1b04736f39b3141cb7
Onur Kucukkece <onurkucukkece@gmail.com>
2018-08-25 12:24:54 +0300

    support for rails 5

--- lib/facebook_dialog/railtie.rb
+++ lib/facebook_dialog/railtie.rb
@@ -3,7 +3,7 @@ require "facebook_dialog/script_helper"
 module FacebookDialog
   class Railtie < Rails::Railtie
     ActiveSupport.on_load(:action_controller) do
-      helper FacebookDialog::ScriptHelper
+      ::ActionController::Base.send(:include, FacebookDialog::ScriptHelper)
       if FacebookDialog.omniauth_enabled?
         require "facebook_dialog/omni_auth_helper"
         helper FacebookDialog::OmniAuthHelper
