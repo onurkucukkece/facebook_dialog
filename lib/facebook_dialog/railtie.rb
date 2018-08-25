77b087a7996e69df5d2af6f381c1c6acd32f6a85
Onur Kucukkece <onurkucukkece@gmail.com>
2018-08-25 13:40:27 +0300

    using helper method with action_controller_base

--- lib/facebook_dialog/railtie.rb
+++ lib/facebook_dialog/railtie.rb
@@ -2,7 +2,7 @@ require "facebook_dialog/script_helper"
 
 module FacebookDialog
   class Railtie < Rails::Railtie
-    ActiveSupport.on_load(:action_controller) do
+    ActiveSupport.on_load(:action_controller_base) do
       helper FacebookDialog::ScriptHelper
       if FacebookDialog.omniauth_enabled?
         require "facebook_dialog/omni_auth_helper"
